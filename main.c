ba4b78c1d04a69e23463285e23ea6ba2c051fdfc
icarus <53394732+Icarus-H@users.noreply.github.com>
2022-05-15 23:36:57 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+#include "./demos/demo1.h"
+#include "./demos/demo2.h"
+#include "./demos/demo3.h"
+#include "./demos/demo4.h"
+#include "./demos/demo5.h"
+#include "./demos/demo6.h"
+#include "./demos/demo7.h"
+#include "./demos/demo8.h"
+#include "./demos/demo9.h"
+
+
+#define DEMO1 1         //LED和跑马灯 按键停止与继续跑马灯
+#define DEMO2 2         //矩阵键盘
+#define DEMO3 3         //串口与SPI实验 数码管
+#define DEMO4 4         //中断管理和CPU计数
+#define DEMO5 5         //LED和跑马灯 矩阵键盘选择功能
+#define DEMO6 6         //看门狗案例程序
+#define DEMO7 7         //作弊秒表
+#define DEMO8 8         //模拟信号AD采集
+#define DEMO8 9         //模拟信号AD采集 中断
+
+#define RUN_DEMO  DEMO9 //选择编译的实验代码
+
+/**
+ * main.c
+ */
+int main(void)
+{
+#if    RUN_DEMO == DEMO1
+    demo1();
+#elif  RUN_DEMO == DEMO2
+    demo2();
+#elif  RUN_DEMO == DEMO3
+    demo3();
+#elif  RUN_DEMO == DEMO4
+    demo4();
+#elif  RUN_DEMO == DEMO5
+    demo5();
+#elif  RUN_DEMO == DEMO6
+    demo6();
+#elif  RUN_DEMO == DEMO7
+    demo7();
+#elif  RUN_DEMO == DEMO8
+    demo8();
+#elif  RUN_DEMO == DEMO9
+    demo9();
+#endif
+
+	return 0;
+}
---
+++ readme.md
@@ -0,0 +1,47 @@
+# DSP_SZU
+深圳大学DSP课程实验代码 个人使用兼存档
+
+## 来源
+[深圳大学 机电学院 2022年春 DSP课程实验 DSP_Labs of CMCE.](https://github.com/team0ne1/SZU_DSP_Labs)
+
+## 环境
+Code Composer Studio Version: 11.2.0.00007
+
+OS: Windows 10
+
+## 程序与实验对照
+demo1:(3) LED和跑马灯 按键停止与继续跑马灯
+
+demo2:(5) 矩阵键盘
+
+demo3:(1) 串口与SPI实验 数码管
+
+demo4:(2) 中断管理和CPU计数
+
+demo5:(3) LED和跑马灯 矩阵键盘选择功能
+
+demo6:(4) 看门狗
+
+demo7:(6) 作弊秒表
+
+demo8:(7) 模拟信号AD采集, 0-3V电压表; 使用扫描法
+
+demo9:(7) 模拟信号AD采集, 0-3V电压表; 使用中断
+
+## 实验列表
+* 串口与SPI实验
+* 中断管理和CPU计数
+* LED和跑马灯
+* 看门狗
+* 矩阵键盘
+* 作弊秒表
+* 模拟信号AD采集 （待完善）
+* EV实验 （未完成）
+* PWM控制电机 （未完成）
+
+## 使用
+main.c文件中 修改 #define RUN_DEMO DEMOx 中的 x 为数字
+
+## 注意事项
+使用GBK编码
+
